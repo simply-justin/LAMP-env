6ad87ff0a9518652f22e0bf30c5119e12604247b
Justin <Justinvdmey@hotmail.com>
2025-06-30 23:58:56 +0200

    Start of documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Default ENV hosts
+127.0.0.1    redis.local
+127.0.0.1    www.redis.local
--- configs/pm2.config.js
+++ configs/pm2.config.js
@@ -1,15 +1,18 @@
-// PM2 process manager configuration file
-// Define your Node.js apps here for process management and monitoring.
-// Example:
-//   apps: [
-//     {
-//       name: 'my-app',
-//       script: './app.js',
-//       instances: 1,
-//       autorestart: true,
-//       watch: false
-//     }
-//   ]
+/**
+ * PM2 Configuration File
+ * ---------------------
+ * This file defines the applications that PM2 should manage.
+ *
+ * Common Options:
+ * - name: Unique identifier for the app
+ * - script: Entry point file
+ * - cwd: Working directory
+ * - instances: Number of instances (use 'max' for cluster mode)
+ * - autorestart: Restart on crash
+ * - watch: Enable file watching
+ * - max_memory_restart: Restart if memory exceeds limit
+ * - env: Environment variables
+ */
 module.exports = {
     apps: [
         // PM2 process configurations
